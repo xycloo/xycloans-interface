8d42d4c90afe46f3e915675032f3560955352e08
Tommaso De Ponti <tommasodeponti@zohomail.eu>
2023-04-19 09:44:38 +0200

    updated explore to 0.5.0

--- src/routes/explore/+page.js
+++ src/routes/explore/+page.js
@@ -5,33 +5,35 @@ import { Buffer } from 'buffer';
 import { TOKENS, TOKENS_MAP } from "../TOKENS";
 
 async function load_vaults() {
-    let server = new SorobanClient.Server("https://rpc-futurenet.stellar.org/")
-    const contractId = "7fd59b4aa2c634157a08727406e37dc8b4a4b68c4ea4e747ea4bf17073f18f6e";
-    let vaults = [];
+  let server = new SorobanClient.Server("https://rpc-futurenet.stellar.org/")
+  const contractId = "03bc11e8d978c6cd257469de2a447f3a4181c3410e79b610c7528db280bd27d8";
+  let vaults = [];
 
-    for (let tok_id of TOKENS) {
 
-	const token_id_key = xdr.ScVal.scvObject(xdr.ScObject.scoVec([xdr.ScVal.scvSymbol("Vault"), xdr.ScVal.scvObject(xdr.ScObject.scoBytes(Buffer.from(tok_id, 'hex')))]));
+  
+  for (let tok_id of TOKENS) {
+    const token_id_key = xdr.ScVal.scvVec([xdr.ScVal.scvSymbol("Vault"), xdr.ScVal.scvBytes(Buffer.from(tok_id, 'hex'))]);
+    
+    let data = await server.getContractData(contractId.toString(), token_id_key);    
+    let from_xdr = SorobanClient.xdr.LedgerEntryData.fromXDR(data.xdr, 'base64');
+    
+    let val = from_xdr.value()._attributes.val.value().toString("hex");
+    
+    vaults.push([val, TOKENS_MAP[tok_id]]);
+  }
 
-	let data = await server.getContractData(contractId, token_id_key);
-
-	let from_xdr = SorobanClient.xdr.LedgerEntryData.fromXDR(data.xdr, 'base64');
-	let val = from_xdr.value()._attributes.val.value().value().toString("hex");
-	vaults.push([val, TOKENS_MAP[tok_id]]);
-    }
-
-    return vaults
+  return vaults
 }
 
 export async function load({ params }) {
-    let server = new SorobanClient.Server("https://rpc-futurenet.stellar.org/")
-    const contractId = "7fd59b4aa2c634157a08727406e37dc8b4a4b68c4ea4e747ea4bf17073f18f6e";
-    let vaults = await load_vaults();
+  let server = new SorobanClient.Server("https://rpc-futurenet.stellar.org/")
+  const contractId = "03bc11e8d978c6cd257469de2a447f3a4181c3410e79b610c7528db280bd27d8";
+  let vaults = await load_vaults();
 
-    return {
-	title: contractId,
-	vaults: vaults,
-    }
-    
-    throw error(404, 'Not found');
+  return {
+    title: contractId,
+    vaults: vaults,
+  }
+
+  throw error(404, 'Not found');
 }
